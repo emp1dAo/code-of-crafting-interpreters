ddc19ee6c716703bea5ac81a54112dac52b0d8a2
scheng <scheng52123@gmail.com>
2021-11-29 11:14:11 +0800

    Fix indents.

--- tool/GenerateAst.java
+++ tool/GenerateAst.java
@@ -14,7 +14,7 @@ public class GenerateAst{
 	}
 	String outputDir = args[0];
 	defineAst(outputDir, "Expr", Arrays.asList(
-	      "Binary   : Expr left, Token operator, Expr right",
+	  "Binary   : Expr left, Token operator, Expr right",
           "Grouping : Expr expression",
           "Literal  : Object value",
           "Unary    : Token operator, Expr right"
@@ -55,7 +55,7 @@ public class GenerateAst{
 
         for (String type : types) {
             String typeName = type.split(":")[0].trim();
-            writer.println("    R visit" + typeName + baseName + "(" +
+            writer.println("        R visit" + typeName + baseName + "(" +
                            typeName + " " + baseName.toLowerCase() + ");");
         }
 
@@ -68,30 +68,30 @@ public class GenerateAst{
         writer.println("    static class " + className + " extends " +
                       baseName + " {");
         // Constructor.
-        writer.println("    " + className + "(" + fieldList + ") {");
+        writer.println("        " + className + "(" + fieldList + ") {");
 
         // Store parameters in fields.
 
         String[] fields = fieldList.split(", ");
         for (String field : fields) {
             String name = field.split(" ")[1];
-            writer.println("    this." + name + " = " + name + ";");
+            writer.println("            this." + name + " = " + name + ";");
         }
 
-        writer.println("    }");
+        writer.println("        }");
 
         // Visitor pattern.
         writer.println();
-        writer.println("    @Override");
-        writer.println("    <R> R accept(Visitor<R> visitor) {");
-        writer.println("      return visitor.visit" +
+        writer.println("        @Override");
+        writer.println("        <R> R accept(Visitor<R> visitor) {");
+        writer.println("            return visitor.visit" +
                        className + baseName + "(this);");
-        writer.println("    }");
+        writer.println("        }");
         
         // Fields.
         writer.println();
         for (String field : fields) {
-            writer.println("    final " + field + ";");
+            writer.println("        final " + field + ";");
         }
         writer.println("    }");
     }
