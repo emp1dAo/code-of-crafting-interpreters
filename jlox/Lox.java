40ab843d470d4e5cb17ddaf73189df7e72ce4bcf
shaochenheng <1010716589@qq.com>
2021-12-01 16:28:49 +0000

    Implement RuntimeError.

--- jlox/Lox.java
+++ jlox/Lox.java
@@ -9,7 +9,10 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
+    
     public static void main(String[] args) throws IOException {
 	if (args.length > 1) {
 	    System.out.println("Usage: jlox [script]");
@@ -28,6 +31,7 @@ public class Lox {
 	run(new String(bytes, Charset.defaultCharset()));
 	// Indicate an error in the exit code.
 	if (hadError) System.exit(65);
+	if (hadRuntimeError) System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -57,10 +61,10 @@ public class Lox {
 	// Stop if ther was syntax error/
 	if (hadError) return;
 
-	System.out.println(new AstPrinter().print(expression));
+	interpreter.interpret(expression);
     }
     // error() and report() helper tells the user some syntax error occurred on a given line.
-    static void error(int line, String message) {
+    static void error(int line, String message)  {
 	report(line, "", message);
     }
 
@@ -77,4 +81,11 @@ public class Lox {
             report(token.line, " at '" + token.lexeme + "'", message);
         }
     }
+
+    static void runtimeError(RuntimeError error) {
+	System.err.println(error.getMessage() +
+			   "\n[line " + error.token.line + "]");
+	hadRuntimeError = true;
+    }
+
 }
