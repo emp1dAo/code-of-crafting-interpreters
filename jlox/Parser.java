a1fb270a191a73d732ca2d6dc839508b9433e957
shaochenheng <1010716589@qq.com>
2022-02-09 01:08:03 +0000

    add PRINT

--- jlox/Parser.java
+++ jlox/Parser.java
@@ -1,5 +1,6 @@
 package com.craftinginterpreters.jlox;
 
+import java.util.ArrayList
 import java.util.List;
 
 import static com.craftinginterpreters.jlox.TokenType.*;
@@ -14,16 +15,35 @@ class Parser {
         this.tokens = tokens;
     }
 
-    Expr parse() {
-        try {
-            return expression();
-        } catch (ParseError error) {
-            return null;
-        }
+    private Expr expression() {
+	return equality();
     }
 
-    private Expr expression() {
-        return equality();
+    private Stmt statement() {
+	if (match(PRINT)) return printStatement();
+	return expressionStatement();
+    }
+
+    private Stmt printStatement() {
+	Expr value = expression();
+	consume(SEMICOLON, "Expect ';' after value.");
+	return new Stmt.Print(value);
+    }
+
+    private Stmt expressionStatement() {
+	Expr expr = expression();
+	consume(SEMICOLON, "Expect ';' after expression.");
+	return new Stmt.Expression(expr);
+    }
+    
+    // This parses a series of statements,
+    // as many as it can find until it hits the end of the input.
+    List<Stmt> parse() {
+	List<Stmt> statements = new ArrayList<>();
+	while (!isAtEnd()) {
+	    statements.add(statement());
+	}
+	return statements;
     }
 
     private Expr equality() {
