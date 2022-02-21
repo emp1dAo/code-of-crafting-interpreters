ccbc7a885b4173092038fe22a48e60a57cdad76b
shaochenheng <1010716589@qq.com>
2022-02-21 22:42:39 +0000

    Add while loop.

--- jlox/Parser.java
+++ jlox/Parser.java
@@ -43,6 +43,7 @@ class Parser {
 
     private Stmt statement() {
 	if (match(PRINT)) return printStatement();
+	if (match(WHILE)) return whileStatement();
        	if (match(LEFT_BRACE)) return new Stmt.Block(block());
 	if (match(IF)) return ifStatement();
 	return expressionStatement();
@@ -83,6 +84,16 @@ class Parser {
 
 	consume(SEMICOLON, "Expect ';' after variable declaration.");
 	return new Stmt.Var(name, initializer);
+    }
+
+    // Actually translate straight tot Java.
+    private Stmt whileStatement() {
+	consume(LEFT_PAREN, "Expect '(' after 'while'.");
+	Expr condition = expression();
+	consume(RIGHT_PAREN, "Expect ')' after condition.");
+	Stmt body = statement();
+
+	return new Stmt.While(condition, body);
     }
 
     private Stmt expressionStatement() {
