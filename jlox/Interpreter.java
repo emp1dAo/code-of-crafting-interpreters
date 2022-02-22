817bde5a87e33919f708f8fabb2f98a5cba033be
shaochenheng <1010716589@qq.com>
2022-02-22 22:59:30 +0000

    Try to add function.

--- jlox/Interpreter.java
+++ jlox/Interpreter.java
@@ -5,7 +5,27 @@ import java.util.ArrayList;
 
 class Interpreter implements Expr.Visitor<Object>,
 			     Stmt.Visitor<Void> {
-    private Environment environment = new Environment();
+    final Environment globals = new Environment();
+
+    // The environment field in the interpreter changes as we enter and exit local scopes. It tracks the current environment.
+    private Environment environment = globals;
+
+    Interpreter() {
+	globals.define("clock", new LoxCallable(){
+		@Override
+		public int arity() { return 0; }
+
+		@Override
+		public Object call(Interpreter interpreter,
+				   List<Object> arguments) {
+		    return (double)System.currentTimeMillis() / 1000.0;
+		}
+
+		@Override
+		public String toString() { return "<native fn>"; }
+	    })
+    }
+    
     void interpret(List<Stmt> statements) {
 	try {
 	    for (Stmt statement : statements) {
@@ -195,6 +215,11 @@ class Interpreter implements Expr.Visitor<Object>,
 	}
 	
 	LoxCallable function = (LoxCallable)callee;
+	if (arguments.size() != function.arity()) {
+	    throw new RuntimeError(expr.paren, "Expected " +
+				   function.arity() + " arguments but got "+
+				   arguments.size() + ".");
+	}
 	return function.call(this, arguments);
     }
 
