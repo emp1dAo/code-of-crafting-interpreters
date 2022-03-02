09a6a0ca78cfecf4d52eb8f8e02fd2d0fb9ace96
shaochenheng <1010716589@qq.com>
2022-03-02 21:48:30 +0000

    Add this and fix bugs.

--- jlox/Resolver.java
+++ jlox/Resolver.java
@@ -17,7 +17,8 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     private enum FunctionType {
 	NONE,
 	FUNCTION,
-	METHOD
+	INITIALIZER,
+	METHOD,
     }
 
     private enum ClassType {
@@ -47,6 +48,9 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 	scopes.peek().put("this", true);
 	for (Stmt.Function method : stmt.methods) {
 	    FunctionType declaration = FunctionType.METHOD;
+	    if (method.name.lexeme.equals("init")) {
+		declaration = FunctionType.INITIALIZER;
+	    }
 	    resolveFunction(method, declaration);
 	}
 	endScope();
@@ -88,6 +92,10 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 	    Lox.error(stmt.keyword, "Can't return from top-level code.");
 	}
 	if (stmt.value != null) {
+	    if (currentFunction == FunctionType.INITIALIZER) {
+		Lox.error(stmt.keyword,
+			  "Can't return a value from an initializer.");
+	    }
 	    resolve(stmt.value);
 	}
 
